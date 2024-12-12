b35f9b0b7b50b165614a99710d0cc5bdbf905169
Konstantin <57441046+HachBoB@users.noreply.github.com>
2024-12-12 13:05:03 +0400

    sosi

---
+++ kp/resources/views/components/footer.blade.php
@@ -0,0 +1,33 @@
+ <footer class="bg-[#007aff] text-white py-8">
+    <div class="container mx-auto px-6 lg:px-20 flex flex-col lg:flex-row justify-between">
+      <!-- About Us -->
+      <div class="mb-6 lg:mb-0">
+        <h4 class="text-lg font-bold text-white mb-2">SportShop</h4>
+        <p class="text-white">
+          Ваш универсальный магазин книг, где вы найдёте всё: от классики до новинок.
+        </p>
+      </div>
+
+      <!-- Navigation Links -->
+      <div class="mb-6 lg:mb-0">
+        <h4 class="text-lg font-bold text-white mb-2">Навигация</h4>
+        <ul>
+          <li><a href="{{ route('home') }}" class="hover:text-yellow-300">Главная</a></li>
+          <li><a href="/about" class="hover:text-yellow-300">О нас</a></li>
+          <li><a href="/where" class="hover:text-yellow-300">Где мы</a></li>
+          <li><a href="{{ route('basket') }}" class="hover:text-yellow-300">Корзина</a></li>
+        </ul>
+      </div>
+
+      <!-- Contact Info -->
+      <div>
+        <h4 class="text-lg font-bold text-white mb-2">Контакты</h4>
+        <p class="text-white">Email: support@SportShop.com</p>
+        <p class="text-white">Телефон: +7 (800) 555-35-35</p>
+        <p class="text-white">Адрес: Москва, ул. Книжная, д. 42</p>
+      </div>
+    </div>
+    <div class="mt-8 text-center text-white">
+      &copy; 2024 SportShop. Все права защищены.
+    </div>
+  </footer>
--- kp/resources/views/layouts/app.blade.php
+++ kp/resources/views/layouts/app.blade.php
@@ -15,6 +15,24 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        @yield("content")
+    <header>
+        <x-header/>
+    </header>
+        <div class="min-h-screen bg-gray-100">
+
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <!-- Page Content -->
+            <main>
+                @yield('content')
+            </main>
+        </div>
     </body>
 </html>
